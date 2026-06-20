973a48185a3bea1b991418965d00455e9cd85ca3
chickej <cowk23@outlook.com>
2026-06-19 21:20:56 -0700

    added

--- src/trigtest.java
+++ src/trigtest.java
@@ -1,9 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class trigtest {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("testing");
+		String[] statements = {
+			"What is sin 0?",
+			"What is sin 30?",
+			"What is sin 60?",
+		};
+
+		Random random = new Random();
+		int randomIndex = random.nextInt(statements.length);
+		String selectedStatement = statements[randomIndex];
+
+		System.out.println(selectedStatement);
 	}
 }
